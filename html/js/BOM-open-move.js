86bac1042f6bf3430e8577b5fb4b9e30a15a8f64
Neel <aidev.neel@gmail.com>
2023-02-21 18:32:57 +0530

    learn css flex-grid-media-position

--- html/js/BOM-open-move.js
+++ html/js/BOM-open-move.js
@@ -2,15 +2,30 @@ var wind;
 
 function openfunc(){
     // wind = window.open("https://google.com","_blank","height=300px, width=300px, left=100px, top=150px");
-    wind = window.open("","","");
-    wind.document.write("<h1>Neel</h1>");
+    wind = window.open("","","width=150px,height=150px");
+    // wind.document.write("<h1>Neel</h1>");
     console.log(wind);
 }
 function clos(){
     wind.close();
 }
 
-function movewin(){
-    wind.moveto(200,200);
-    wind.focus();
+function movetofun(){
+    wind.moveTo(200,200);
+    
+}
+
+function movebyfun(){
+    wind.moveBy(200,200);
+   
+}
+
+function resizetofun(){
+    wind.resizeTo(400,400);
+  
+}
+
+function resizebyfun(){
+    wind.resizeBy(400,400);
+  
 }
---
+++ html/js/BOM-scroll.js
@@ -0,0 +1,15 @@
+function scrollwinup(){
+    window.scrollBy(0,-1000);
+}
+
+function scrollwindown(){
+    window.scrollBy(0,500);
+}
+
+function scrollwinright(){
+    window.scrollBy(200,0);
+}
+
+function scrollwinleft(){
+    window.scrollBy(-200,0);
+}
